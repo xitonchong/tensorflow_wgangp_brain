27683f8f266a33412dd74d4aac84421b87dc0b03
xitonchong <xitonchong@gmail.com>
2019-05-10 16:09:43 +0800

    run file diversify

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python main.py  --test=True --train=True --data_dir=/tf/work/cluster/server1/home/xitonchong/data  --batch_size=2  --use_resnet=True  
+python main.py  --test=True --train=True --data_dir=/tf/work/cluster/server1/home/xitonchong/data  --batch_size=2  --use_g_resnet=True  
---
+++ run_gResNetDsicDCGAN.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python main.py  --test=True --train=True --data_dir=/tf/work/cluster/server1/home/xitonchong/data  --batch_size=2  --use_g_resnet=True  --use_d_resnet=False

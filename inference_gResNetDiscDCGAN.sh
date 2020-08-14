39bb414786c25fe1f653cfb73fe4f8d5a2dc6a1c
xitonchong <xitonchong@gmail.com>
2020-08-14 14:21:00 +0800

    example script to generate deformation field

---
+++ inference_gResNetDiscDCGAN.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+python main.py --generate_test_images=1100  --sample_dir=./run1_gresNet --train=False --test=True --use_g_resnet=True  --use_d_resnet=False --model_dir=genResNet_model
+
